c244ec700fe1b4e08dd97506a5382a78b0ae9dbe
mysmster <mysmster@gmail.com>
2017-03-25 22:51:28 +0300

    Чтение списка огранизаций

---
+++ ParseHTML.py
@@ -0,0 +1,19 @@
+import lxml.html
+import requests
+
+#page = requests.get('https://moscowinfo24.ru/prodazha-i-obsluzhivanie-liftov-v-moskve/').text
+page = open('c:\\downloads\\lifts.htm',encoding='utf-8').read()
+document = lxml.html.fromstring(page)
+
+firm_list_lxml = document.xpath('//a[@class = "title"]')
+for item_lxml in firm_list_lxml:
+    firm_name = item_lxml.xpath('text()')[0]
+    firm_link =  item_lxml.xpath('@href')[0]
+    print ('%s %s'%(firm_name,firm_link))
+
+    # парсим карточку фирмы
+    page_firm = requests.get(firm_link).text
+    document_firm = lxml.html.fromstring(page)
+    firm_cart_lxml = document.xpath('//ul[@class = "listoption"]')
+    firm_site = document.xpath('//id("description")/x:div[1]/x:ul/x:li[6]')
+    print (firm_site)
