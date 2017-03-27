e59662afd011001b06478bd60346f6fe3dc2aa55
mysm <mysm@mail.ru>
2017-03-27 16:17:18 +0300

    Парсинг карточки при помощи BeautifulSoup

--- ParseHTML.py
+++ ParseHTML.py
@@ -1,20 +1,26 @@
-import lxml.htmlfrom bs4 import BeautifulSoup
+import lxml.html
+from bs4 import BeautifulSoup
 import requests
 
-#page = requests.get('https://moscowinfo24.ru/prodazha-i-obsluzhivanie-liftov-v-moskve/').text
-page = open('c:\\downloads\\lifts2.htm',encoding='utf-8').read()
+# page = requests.get('https://moscowinfo24.ru/prodazha-i-obsluzhivanie-liftov-v-moskve/').text
+page = open('c:\\downloads\\lifts2.htm', encoding='utf-8').read()
 document = lxml.html.fromstring(page)
 
-#firm_list_lxml = document.xpath('//a[@class = "title"]')
+# firm_list_lxml = document.xpath('//a[@class = "title"]')
 firm_list_lxml = document.xpath('//h4[@class = "job-title"]/a')
 for item_lxml in firm_list_lxml:
     firm_name = item_lxml.xpath('text()')[0]
-    firm_link =  item_lxml.xpath('@href')[0]
-    print ('%s %s'%(firm_name,firm_link))
+    firm_link = item_lxml.xpath('@href')[0]
+    print('%s %s' % (firm_name, firm_link))
 
     # парсим карточку фирмы
     page_firm = requests.get(firm_link).text
-    document_firm = lxml.html.fromstring(page_firm)
-    firm_cart_lxml = document.xpath('//i[@class = "icon-map"]')
-    for i in firm_cart_lxml:
-        print(i.xpath('/li/text()')[0])
+    soup = BeautifulSoup(page_firm)
+    firm_cart_soup = soup.find('div', {'class': 'org-Details'})
+    firm_cart_address = soup.find('ul', {'class': 'list-border'}).find('li').text
+    firm_cart_li = soup.find_all('ul', {'class': 'list-border'})
+
+    # document_firm = lxml.html.fromstring(page_firm)
+    # firm_cart_lxml = document.xpath('//div[@class = "org-Details"]')
+    # for i in firm_cart_lxml:
+    #     print(i.xpath('/li/text()')[0])
