e469f0181be7176f38c530ddffc4d0c9c21f4042
mysmster <mysmster@gmail.com>
2017-04-02 20:54:35 +0300

    Добавлен поиск email компании на сайте. Версия1

--- ParseHTML.py
+++ ParseHTML.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import re
+
+def GetFirmEMail(firm_site):
+    # получаем главную страницу
+    # ищем на ней почту
+    # если не находим, ищем ссылку контакты
+
+    main_page_firm = requests.get(firm_site).text
+    soup = BeautifulSoup(page_firm, "html.parser")
+    re.findall("/.+@.+\..+/i",main_page_firm)
+
+    return ""
 
 output_file = open("lifts.csv", "wb")
 csv_out = csv.writer(output_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
@@ -25,6 +37,7 @@ for item_lxml in firm_list_lxml:
     firm_cart_li = soup.find('ul', {'class': 'list-border'})
     for i in firm_cart_li.findAll('li'):
         i_text = i.text.strip()
+        # нужно только первое двоеточие, останые должны остаться в val
         pos = i_text.find(":")
         par = str(i_text[:pos])
         val = str(i_text[pos:])
@@ -37,7 +50,8 @@ for item_lxml in firm_list_lxml:
         elif par == "Email":
             firm_email = val
 
-
+    if (firm_email == "") and (firm_site != ""):
+        firm_email = GetFirmEMail(firm_site)
     # csv_out.writerow([firm_name.encode('utf-8'), firm_address.encode("utf-8"), firm_phones.encode("utf-8"),
     #                   firm_site.encode("utf-8"), firm_email.encode("utf-8")])
     print("%s\t%s\t%s\t%s\t%s"%(firm_name,firm_address,firm_phones,firm_site,firm_email))
